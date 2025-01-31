6f77a7965d0460203d2e97b06b969bcf2a66cd6b
zvm23 <zhenyuch@usc.edu>
2025-01-30 16:08:07 -0800

    upload data retrieving script with database connection function

---
+++ lab3/script/yfinance_retrieve.py
@@ -0,0 +1,15 @@
+﻿import sys
+import datetime
+import sqlalchemy
+import pymysql
+import pandas as pd
+import yfinance as yf
+
+my_db = sqlalchemy.create_engine("mysql+pymysql://root:yiwen960131@localhost/dsci560")
+
+def query():
+	data = pd.read_sql("SELECT * FROM Course", my_db)
+	print(data)
+
+if __name__ == "__main__":
+	query()
