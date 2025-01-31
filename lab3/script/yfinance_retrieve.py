0e13559b8c0cb31587d02c37c90bdffd71f7cd76
zvm23 <zhenyuch@usc.edu>
2025-01-30 16:19:06 -0800

    update database connection function of data retrieving script

--- lab3/script/yfinance_retrieve.py
+++ lab3/script/yfinance_retrieve.py
@@ -5,11 +5,15 @@ import pymysql
 import pandas as pd
 import yfinance as yf
 
-my_db = sqlalchemy.create_engine("mysql+pymysql://root:yiwen960131@localhost/dsci560")
 
 def query():
 	data = pd.read_sql("SELECT * FROM Course", my_db)
 	print(data)
 
+
 if __name__ == "__main__":
+	db_password = input("Please enter the password for the database: ")
+	db_name = input("Please enter the database name: ")
+	my_db = sqlalchemy.create_engine(f"mysql+pymysql://root:{db_password}@localhost/{db_name}")
+
 	query()
