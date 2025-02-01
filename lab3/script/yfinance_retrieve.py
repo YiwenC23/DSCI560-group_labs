2b1f63cc609b0fef2d1925b2fce95decd885fb08
zvm23 <zhenyuch@usc.edu>
2025-02-01 15:15:47 -0800

    update code of index creations in connect_db function.

--- lab3/script/yfinance_retrieve.py
+++ lab3/script/yfinance_retrieve.py
@@ -5,15 +5,11 @@ import yfinance as yf
 from sqlalchemy import text
 
 
-def connect_db():
+def connect_db(db_username, db_password, db_name):
     try:
-        db_username = input("Please enter the username for the database: ")
-        db_password = input("Please enter the password for the database: ")
-        db_name = input("Please enter the database name: ")
         engine = sqlalchemy.create_engine(f"mysql+pymysql://{db_username}:{db_password}@localhost/{db_name}")
         
         with engine.connect() as conn:
-            
             conn.execute(text(" \
                 CREATE TABLE IF NOT EXISTS stock_data( \
                     date DATE NOT NULL, \
@@ -26,17 +22,31 @@ def connect_db():
                     PRIMARY KEY (date, ticker) \
                     ); \
             "))
-            '''
-            conn.execute(text(" \
-                CREATE INDEX IF NOT EXISTS idx_ticker \
-                ON stock_data (ticker); \
-            "))
             
-            conn.execute(text(" \
-                CREATE INDEX IF NOT EXISTS idx_date \
-                ON stock_data (date); \
-            "))
-        '''
+            # Check if the index already exists
+            check_ticker_index = conn.execute(text(" \
+                            SELECT COUNT(1) indexExists \
+                                FROM INFORMATION_SCHEMA.STATISTICS \
+                                WHERE table_schema = 'dsci560' \
+                                    AND table_name = 'stock_data' \
+                                    AND index_name = 'idx_ticker';"))
+            if check_ticker_index.fetchone()[0] == 0:
+                conn.execute(text("CREATE INDEX idx_ticker ON stock_data (ticker);"))
+            else:
+                print("Index already exists")
+            
+            # Check if the date index already exists
+            check_date_index = conn.execute(text(" \
+                            SELECT COUNT(1) indexExists \
+                                FROM INFORMATION_SCHEMA.STATISTICS \
+                                WHERE table_schema = 'dsci560' \
+                                    AND table_name='stock_data' \
+                                    AND index_name='idx_date';"))
+            if check_date_index.fetchone()[0] == 0:
+                conn.execute(text("CREATE INDEX idx_date ON stock_data (date)"))
+            else:
+                print("Index already exists")
+        
         print("Database connected successfully")
         return engine
     
@@ -52,9 +62,6 @@ def stock_retrieve():
         hist_data = pd.DataFrame()
         for i in tickers:
             ticker = yf.Ticker(i)
-        
-            # Get all available information
-#            info = aapl.info
             
             # Get historical price data
             tck_data = ticker.history(start="2025-01-20", end="2025-01-30", interval="1d")
@@ -68,19 +75,34 @@ def stock_retrieve():
             # Sort the data by Date and Ticker
             hist_data = hist_data.sort_values(['Date', 'Ticker'])
         
+        # Get all available information
+#        info = aapl.info
+        
         # Get additional data
 #        dividends = aapl.dividends
+        
 #        splits = aapl.splits
 #        actions = aapl.actions
+        
         print("Stock data retrieved successfully.")
         return hist_data
+    
     except Exception as e:
         print(e)
-        return None
+        sys.exit()
 
 
 def insert_db(engine, data):
     try:
+        # Check if the data is already in the database
+        check_data = engine.execute(text(" \
+                            SELECT COUNT(1) dataExists \
+                                FROM stock_data \
+                                WHERE date = :date AND ticker = :ticker;"),
+                                date=data.index[0], ticker=data.iloc[0]['Ticker'])
+        if check_data.fetchone()[0] > 0:
+            print("Data already exists in the database")
+            return
         
         # Write to database
         data.to_sql(
@@ -97,7 +119,10 @@ def insert_db(engine, data):
 
 if __name__ == "__main__":
 #    ticker = ["AAPL", "MSFT", "AMZN", "NVDA", "META", "TSLA"]
-    db_engine = connect_db()
+    db_username = input("Please enter the username for the database: ")
+    db_password = input("Please enter the password for the database: ")
+    db_name = input("Please enter the database name: ")
+    db_engine = connect_db(db_username, db_password, db_name)
     
     raw_data = stock_retrieve()
     
