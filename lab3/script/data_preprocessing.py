bfb6fb5204d94cfe84303638080b46fd48b31bde
HanluMaa <melodymamhl@outlook.com>
2025-01-31 21:45:14 -0800

    Update data_preprocessing.py
    Fix some bugs for data_preprocessing.py

--- lab3/script/data_preprocessing.py
+++ lab3/script/data_preprocessing.py
@@ -4,31 +4,7 @@ import sqlalchemy
 import pymysql
 import pandas as pd
 import yfinance as yf
-
-# def create_connection():
-#     connection = mysql.connector.connect(
-#         host='localhost',
-#         user='root',
-#         password='!Mhl020725'
-#     )
-#     return connection
-
-# def fetch_stock_data(connection, ticker):
-#     query = f"SELECT * FROM stocks WHERE ticker = '{ticker}' ORDER BY date;"
-    
-#     df = pd.read_sql(query, con=connection) 
-
-#     return df
-
-# connection = create_connection()
-
-# if connection:
-#     ticker = 'AAPL'
-#     df = fetch_stock_data(connection, ticker)
-    
-#     print(df.head())
-
-#     connection.close() 
+from sqlalchemy import create_engine
 
 def query():
 	data = pd.read_sql("SELECT * FROM stock_data", my_db)
@@ -42,19 +18,19 @@ def missing_data(df):
     return df
 
 def daily_return(df):
-    df['Daily Return'] = df.groupby('Ticker')['Close'].pct_change() * 100  
+    df['Daily Return'] = df.groupby('ticker')['close'].pct_change() * 100  
     return df
 
 def volatility(df):
-    df['Volatility'] = df.groupby('Ticker')['Daily Return'].rolling(window=20).std() * (252 ** 0.5)
+    df['Volatility'] = df.groupby('ticker')['Daily Return'].rolling(window=20).std().reset_index(level=0, drop=True) * (252 ** 0.5)
     return df
 
 def moving_average(df, window):
-    df['Moving Average'] = df.groupby('Ticker')['Close'].rolling(window=window).mean().reset_index(level=0, drop=True)
+    df['Moving Average'] = df.groupby('ticker')['close'].rolling(window=window).mean().reset_index(level=0, drop=True)
     return df
 
 def VWAP(df):
-    df['VWAP'] = df.groupby('Ticker').apply(lambda x: (x['Close'] * x['Volume']).cumsum() / x['Volume'].cumsum()).reset_index(level=0, drop=True)
+    df['VWAP'] = (df['close']*df['volume']).groupby(df['ticker']).cumsum() / df['volume'].groupby(df['ticker']).cumsum()
     return df
 
 
@@ -64,12 +40,12 @@ if __name__ == "__main__":
     db_name = input("Please enter the database name: ")
     my_db = sqlalchemy.create_engine(f"mysql+pymysql://{db_user}:{db_password}@localhost/{db_name}")
 
-    connection = engine.connect()
+    connection = my_db.connect()
 
     query()
 
     stock_data = pd.read_sql_table('stock_data', connection)
-    stock_data['Date'] = pd.to_datetime(stock_data['Date'])
+    stock_data['date'] = pd.to_datetime(stock_data['date'])
 
     stock_data = missing_data(stock_data)
 
@@ -80,3 +56,5 @@ if __name__ == "__main__":
     stock_data = moving_average(stock_data, 20)
 
     stock_data = VWAP(stock_data)
+
+    print(stock_data)
