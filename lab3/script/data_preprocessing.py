5c6d9daab7315ccf5bc2a02bf974c24060ea4f55
zvm23 <zhenyuch@usc.edu>
2025-02-01 17:07:37 -0800

    update script with function importing.

--- lab3/script/data_preprocessing.py
+++ lab3/script/data_preprocessing.py
@@ -5,6 +5,7 @@ import pymysql
 import pandas as pd
 import yfinance as yf
 from sqlalchemy import create_engine
+from yfinance_retrieve import connect_db
 
 def missing_data(df):
     for column in df.columns:
@@ -31,14 +32,9 @@ def VWAP(df):
 
 
 if __name__ == "__main__":
-    db_user = input("Please enter the username for the database: ")
-    db_password = input("Please enter the password for the database: ")
-    db_name = input("Please enter the database name: ")
-    my_db = sqlalchemy.create_engine(f"mysql+pymysql://{db_user}:{db_password}@localhost/{db_name}")
+    engine = connect_db()
 
-    connection = my_db.connect()
-
-    stock_data = pd.read_sql_table('stock_data', connection)
+    stock_data = pd.read_sql_table('stock_data', engine)
     stock_data['date'] = pd.to_datetime(stock_data['date'])
 
     stock_data = missing_data(stock_data)
