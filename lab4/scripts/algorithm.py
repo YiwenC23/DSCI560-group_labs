3016ddf38fd313955e7cbb7ea2b5c875c5ec83e1
HanluMaa <melodymamhl@outlook.com>
2025-02-07 22:32:17 -0800

    Update algorithm.py
    Update algorithm.py to include decision signal

--- lab4/scripts/algorithm.py
+++ lab4/scripts/algorithm.py
@@ -73,7 +73,7 @@ def moving_average(data, window):
     return pd.Series(data).rolling(window=window).mean()
 
 def moving_average_prediction(df, column, window):
-    df[f"MA_{window}"] = moving_average(df[column], window)
+    df["MA"] = moving_average(df[column], window)
     return df
 
 def exponential_smoothing(data, trend):
@@ -120,6 +120,49 @@ def model_evaluation(actual, predicted, model_name, dict):
 
     return dict
 
+def decision_signal(metrics, stock_data, ARIMA_model):
+    mae_dict = {key: value for key, value in metrics.items() if 'MAE' in key}
+    rmse_dict = {key: value for key, value in metrics.items() if 'RMSE' in key}
+
+    mae_dict = sorted(mae_dict.items(), key=lambda item: item[1])
+    rmse_dict = sorted(rmse_dict.items(), key=lambda item: item[1])
+
+    model_names = ['MA', 'ARIMA', 'ES']
+    rank = 1
+    rank_dict = {'MA':0, 'ARIMA':0, 'ES':0}
+
+    for model in model_names:
+        for key in mae_dict.keys():
+            if model in key:
+                rank_dict[key] += rank
+                rank += 1
+        for key in rmse_dict.keys():
+            if model in key:
+                rank_dict[key] += rank
+                rank += 1
+    
+    best_model = min(rank_dict, key=rank_dict.get)
+
+    if best_model == 'MA':
+        prediction = stock_data['close'].iloc[-3:].mean()
+    elif best_model == 'ES':
+        last_10_data = stock_data['close'].iloc[-10:]
+        ES = exponential_smoothing(last_10_data, trend='add')
+        prediction = exponential_smoothing_prediction(ES, steps=1)
+    elif best_model == 'ARIMA':
+        prediction = autoarima_prediction(ARIMA_model, steps = 1)
+
+
+    past_20_day_avg = stock_data['close'].iloc[-20:].mean()
+
+    if prediction > past_20_day_avg:
+        return 'sell'
+    elif prediction < past_20_day_avg:
+        return 'buy'
+    else:
+        return 'no move'
+
+
 if __name__ == "__main__":
     connect_db()
     stock_data = get_DBdata()
@@ -153,7 +196,7 @@ if __name__ == "__main__":
 
     # moving average
     MA_prediction = moving_average_prediction(stock_data, 'close', 3)
-    metrics = model_evaluation(stock_data['close'], stock_data['MA_3'], 'MA_3', metrics)
+    metrics = model_evaluation(stock_data['close'], stock_data['MA'], 'MA', metrics)
 
     # exponential smoothing
     stock_data["ES"] = np.nan
@@ -166,12 +209,12 @@ if __name__ == "__main__":
     metrics = model_evaluation(stock_data['close'], stock_data['ES'], 'ES', metrics)
 
     # auto-arima
-    stock_data["ARIMA1"] = np.nan
+    stock_data["ARIMA"] = np.nan
     ARIMA = autoarima(training_data)
     ARIMA_predictions = autoarima_prediction(ARIMA, steps = len(testing_data))
     for i, pred in enumerate(ARIMA_predictions):
-        stock_data.iloc[len(training_data)+i, stock_data.columns.get_loc("ARIMA1")] = pred
+        stock_data.iloc[len(training_data)+i, stock_data.columns.get_loc("ARIMA")] = pred
 
-    metrics = model_evaluation(stock_data['close'], stock_data['ARIMA1'], 'ARIMA1', metrics)
+    metrics = model_evaluation(stock_data['close'], stock_data['ARIMA'], 'ARIMA', metrics)
 
-    print(metrics)
+    
