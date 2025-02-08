0286244fa776ebd947d406b4e2de33ee3700d49d
zvm23 <zhenyuch@usc.edu>
2025-02-08 07:26:18 -0800

    Added algorithm for certificates verification.

--- lab4/scripts/database.py
+++ lab4/scripts/database.py
@@ -1,5 +1,6 @@
 ﻿import sys
 import sqlalchemy as sql
+from sqlalchemy import text
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 Base = declarative_base()
@@ -79,35 +80,36 @@ class UserAsset(Base):
 
 #* Define the function to connect to the database
 def connect_db():
-    
-    #? Get the database credentials from the user
-    db_username = input("Please enter the username for the database: ")
-    db_password = input("Please enter the password for the database: ")
-    db_name = input("Please enter the database name: ")
-    
-    conn_url = f"mysql+pymysql://{db_username}:{db_password}@localhost/{db_name}"
-    
-    try:
+    while True:
+        #? Get the database credentials from the user
+        db_username = input("[System] Please enter the username for the database: ")
+        db_password = input("[System] Please enter the password for the database: ")
+        db_name = input("[System] Please enter the database name: ")
         
-        print(f"Establishing connection to {db_name} database as {db_username}...")
-        #? Create database engine with connection pool configuration
-        engine = sql.create_engine(
-            conn_url,
-            pool_size=20,     # Number of maintained idle connections in the pool
-            pool_recycle=3600,    # Recycle connections hourly to prevent connection timeout
-            max_overflow=10,    # Allow up to 10 additional connections to the pool
-            pool_pre_ping=True,     # Validate connection viability before use
-            echo=False    # Disable engine logging
-            )
-        
-
-        
-        print("Successfully connected to the database!")
+        conn_url = f"mysql+pymysql://{db_username}:{db_password}@localhost/{db_name}"
     
-        return engine
-    except Exception as e:
-        print(f"Connection failed: {e}")
-        raise
+        try:
+            
+            print(f"\n[System] Establishing connection to {db_name} database as {db_username}...")
+            #? Create database engine with connection pool configuration
+            engine = sql.create_engine(
+                conn_url,
+                pool_size=20,     # Number of maintained idle connections in the pool
+                pool_recycle=3600,    # Recycle connections hourly to prevent connection timeout
+                max_overflow=10,    # Allow up to 10 additional connections to the pool
+                pool_pre_ping=True,     # Validate connection viability before use
+                echo=False    # Disable engine logging
+                )
+            
+            with engine.connect() as conn:
+                conn.execute(text("SELECT 1"))
+            
+            print("\n[INFO] Successfully connected to the database!")
+            return engine
+        
+        except Exception as e:
+            print(f"\n[ERROR] Connection failed: {e}")
+            print("\n[INFO] Please check your credentials and try again.\n")
 
 #? Create the engine
 engine = connect_db()
