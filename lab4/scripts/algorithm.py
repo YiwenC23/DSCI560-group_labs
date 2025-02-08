ecad4c8bd09c36ef30c56c6c5d1953e9d9d5a83b
zvm23 <zhenyuch@usc.edu>
2025-02-07 22:48:40 -0800

    updated scritp.

--- lab4/scripts/algorithm.py
+++ lab4/scripts/algorithm.py
@@ -2,7 +2,6 @@ import sys
 import pandas as pd
 import yfinance as yf
 import sqlalchemy as sql
-from sqlalchemy.orm import declarative_base, sessionmaker
 import matplotlib.pyplot as plt
 from yfinance_retrieve import StockData
 from statsmodels.graphics.tsaplots import plot_acf
@@ -12,55 +11,10 @@ from statsmodels.tsa.holtwinters import ExponentialSmoothing
 import numpy as np
 import warnings
 
+from database import SessionLocal
+
 warnings.filterwarnings('ignore')
 
-Base = declarative_base()
-
-#* Define the function to connect to the database
-def connect_db():
-    global engine, SessionLocal
-    try:
-        #? Get the database credentials from the user
-        db_username = input("Please enter the username for the database: ")
-        db_password = input("Please enter the password for the database: ")
-        db_name = input("Please enter the database name: ")
-        
-        print(f"Establishing connection to {db_name} database as {db_username}...")
-        #? Create database engine with connection pool configuration
-        engine = sql.create_engine(
-            f"mysql+pymysql://{db_username}:{db_password}@localhost/{db_name}",
-            pool_size=20,     # Number of maintained idle connections in the pool
-            pool_recycle=3600,    # Recycle connections hourly to prevent connection timeout
-            max_overflow=10,    # Allow up to 10 additional connections to the pool
-            pool_pre_ping=True,     # Validate connection viability before use
-            echo=False    # Disable engine logging
-            )
-        
-        #? Create the Session Library
-        SessionLocal = sessionmaker(
-            bind=engine,
-            autocommit=False,    # Require explicit commit() for transaction control
-            autoflush=False,    # Delay SQL emission until flush()/commit() called, enables batch operations
-            expire_on_commit=False,    # Keep object attributes accessible after commit
-            class_=sql.orm.Session    # Use the SQLAlchemy Session class
-        )
-        
-        #? Initialize database schema if not exists
-        Base.metadata.create_all(bind=engine)
-        
-        print("Successfully connected to the database!")
-    
-    except Exception as e:
-        print(f"Connection failed: {e}")
-        sys.exit(1)
-
-
-def get_DBdata():
-    with SessionLocal() as session:
-        stock_data = session.query(StockData).all()
-        df = pd.DataFrame([s.__dict__ for s in stock_data])
-        df= df.drop(columns=["_sa_instance_state"], errors="ignore")
-        return df
 
 def missing_data(df):
     for column in df.columns:
@@ -164,8 +118,6 @@ def decision_signal(metrics, stock_data, ARIMA_model):
 
 
 if __name__ == "__main__":
-    connect_db()
-    stock_data = get_DBdata()
 
     stock_data['date'] = pd.to_datetime(stock_data['date'])
     stock_data = missing_data(stock_data)
