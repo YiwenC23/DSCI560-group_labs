953430224baf61bd6fdcd25e582f9fb8d7aeb38f
HanluMaa <melodymamhl@outlook.com>
2025-02-08 23:26:37 -0800

    fix some small details for algorithm.py

--- lab4/scripts/algorithm.py
+++ lab4/scripts/algorithm.py
@@ -1,10 +1,8 @@
 import sys
 import pandas as pd
-import yfinance as yf
 import sqlalchemy as sql
-from sqlalchemy.orm import declarative_base, sessionmaker
 import matplotlib.pyplot as plt
-from yfinance_retrieve import StockData
+from database import StockData, SessionLocal
 from statsmodels.graphics.tsaplots import plot_acf
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 import pmdarima as pm
@@ -13,47 +11,6 @@ import numpy as np
 import warnings
 
 warnings.filterwarnings('ignore')
-
-Base = declarative_base()
-
-#* Define the function to connect to the database
-def connect_db():
-    global engine, SessionLocal
-    try:
-        #? Get the database credentials from the user
-        db_username = input("Please enter the username for the database: ")
-        db_password = input("Please enter the password for the database: ")
-        db_name = input("Please enter the database name: ")
-        
-        print(f"Establishing connection to {db_name} database as {db_username}...")
-        #? Create database engine with connection pool configuration
-        engine = sql.create_engine(
-            f"mysql+pymysql://{db_username}:{db_password}@localhost/{db_name}",
-            pool_size=20,     # Number of maintained idle connections in the pool
-            pool_recycle=3600,    # Recycle connections hourly to prevent connection timeout
-            max_overflow=10,    # Allow up to 10 additional connections to the pool
-            pool_pre_ping=True,     # Validate connection viability before use
-            echo=False    # Disable engine logging
-            )
-        
-        #? Create the Session Library
-        SessionLocal = sessionmaker(
-            bind=engine,
-            autocommit=False,    # Require explicit commit() for transaction control
-            autoflush=False,    # Delay SQL emission until flush()/commit() called, enables batch operations
-            expire_on_commit=False,    # Keep object attributes accessible after commit
-            class_=sql.orm.Session    # Use the SQLAlchemy Session class
-        )
-        
-        #? Initialize database schema if not exists
-        Base.metadata.create_all(bind=engine)
-        
-        print("Successfully connected to the database!")
-    
-    except Exception as e:
-        print(f"Connection failed: {e}")
-        sys.exit(1)
-
 
 def get_DBdata():
     with SessionLocal() as session:
@@ -138,9 +95,14 @@ def decision_signal(metrics, stock_data, ARIMA_model):
 def algorithm(stock_data):
     try:
         stock_data['date'] = pd.to_datetime(stock_data['date'])
-        stock_data = missing_data(stock_data)
+        # Filter the data to only include the last 2 years
+        stock_data = stock_data[stock_data.index >= (pd.Timestamp.now() - pd.Timedelta(days=730))]
         stock_data = stock_data[['date', 'close']]
+        stock_data = missing_data(stock_data)
         stock_data.set_index('date', inplace=True)
+        
+        #? Filter the data to only include the last 2 years
+        stock_data = stock_data[stock_data.index >= (pd.Timestamp.now() - pd.Timedelta(days=730))]
 
         # determine the seasonality/trend feature of the stock price
         # plot_acf(stock_data['close'], lags=52)  # Adjust lags as needed
@@ -187,12 +149,4 @@ def algorithm(stock_data):
 
     except Exception as e:
         print(f"Failed to run time series forecasting algorithm: {e}")
-        sys.exit(1)
 
-if __name__ == "__main__":
-    connect_db()
-    stock_data = get_DBdata()
-
-    # get stock_data
-    signal = algorithm(stock_data)
-    print(signal)
