7e0c8f86a66df386eb735e4ab66fac4af1a31780
HanluMaa <melodymamhl@outlook.com>
2025-02-22 21:53:35 -0800

    Update web_scraper.py

--- lab6/scripts/web_scraper.py
+++ lab6/scripts/web_scraper.py
@@ -6,7 +6,7 @@ from sqlalchemy import update
 import time
 import re
 import string
-from test_database import WellStatus, SessionLocal
+from database import WellStatus, SessionLocal
 from lxml import etree
 
 def separate_and_lowercase(text):
@@ -40,10 +40,10 @@ def construct_well_url(state, county, well_name, api_number):
 
 # Scrape well details from the dynamically generated URL.
 def scrape_well_data(state, county, well_name, api_number):
-    # ABBR_TREE = etree.parse("Abbreviations.xml")
+    ABBR_TREE = etree.parse("Abbreviations.xml")
 
-    # state = tokenize_phrase(state, ABBR_TREE)
-    # county = tokenize_phrase(county, ABBR_TREE)
+    state = tokenize_phrase(state, ABBR_TREE)
+    county = tokenize_phrase(county, ABBR_TREE)
 
     well_url = construct_well_url(state, county, well_name, api_number)
     headers = {
