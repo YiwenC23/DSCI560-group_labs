166c34a83d070d526f062637286162e1264f59fb
zvm23 <zhenyuch@usc.edu>
2025-02-28 18:28:02 -0800

    update scripts

---
+++ lab7/scripts/db.js
@@ -0,0 +1,20 @@
+﻿var mysql = require("mysql2");
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "yiwen960131",
+    authPlugins: {
+        caching_sha2_password: () => require('mysql2/lib/auth/caching_sha2_password')
+    }
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+con.query("SELECT * FROM well_info", function (err, result) {
+    if (err) throw err;
+    console.log(result);
+});
